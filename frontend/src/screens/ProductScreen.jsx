88ced40dd771dc1ed2361537c8667e187f7f3b82
Hamid <hamidtahir2000@gmail.com>
2022-10-23 20:55:52 +0500

    C3L03

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
 import { Col, Row, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 function ProductScreen() {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [id])
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
