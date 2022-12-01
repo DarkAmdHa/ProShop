8866dce2c6efa5fb6cf99aa10a935fab44cae88f
Hamid <hamidtahir2000@gmail.com>
2022-12-01 14:38:37 +0500

    Another One

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from 'express'
-import products from './data/products.js'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -7,17 +15,16 @@ app.get('/', (req, res) => {
   res.send('ProShop API')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  if (!product) {
-    res.json({ message: 'No product with given ID in database.' })
-  }
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
-app.listen('5000', () => {
-  console.log('Server Running On Port 5000')
+app.use(notFound)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+  )
 })
