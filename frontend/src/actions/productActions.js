ad46ee003fe3bd9f6e2111d0f21b0d947640d5fd
Hamid <hamidtahir2000@gmail.com>
2022-12-06 12:00:46 +0500

    Another One

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({ type: 'PRODUCT_LIST_REQUEST' })
+    dispatch({ type: PRODUCT_LIST_REQUEST })
     const { data } = await axios.get('/api/products/')
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,19 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import {
   productDetailsReducer,
   productListReducer,
-} from './reducers/productReducer'
+} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
-const initialState = {}
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+const initialState = { cart: { cartItems: cartItemsFromStorage } }
 const middleware = [thunk]
 
 const store = createStore(
